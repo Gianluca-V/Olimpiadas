258f7a3fbcef14975e4574402f1485e829dd4445
Gianluca-V <107764977+Gianluca-V@users.noreply.github.com>
2023-09-21 17:09:25 -0300

    added DB

---
+++ hospital.sql
@@ -0,0 +1,177 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 21-09-2023 a las 22:09:13
+-- Versión del servidor: 10.4.19-MariaDB
+-- Versión de PHP: 8.0.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `hospital`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `areas`
+--
+
+CREATE TABLE `areas` (
+  `ID` int(11) NOT NULL,
+  `Name` varchar(99) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `calls`
+--
+
+CREATE TABLE `calls` (
+  `ID` int(11) NOT NULL,
+  `Type` varchar(10) NOT NULL,
+  `ResponseTime` int(11) NOT NULL,
+  `Attended` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `nurses`
+--
+
+CREATE TABLE `nurses` (
+  `ID` int(11) NOT NULL,
+  `FirstName` varchar(99) NOT NULL,
+  `LastName` varchar(99) NOT NULL,
+  `DNI` int(8) NOT NULL,
+  `Phone` int(15) DEFAULT NULL,
+  `Address` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `patients`
+--
+
+CREATE TABLE `patients` (
+  `ID` int(11) NOT NULL,
+  `FirstName` varchar(99) NOT NULL,
+  `LastName` varchar(99) NOT NULL,
+  `DNI` int(8) NOT NULL,
+  `Phone` int(15) DEFAULT NULL,
+  `Address` text DEFAULT NULL,
+  `BirthDate` varchar(10) DEFAULT NULL,
+  `Location` int(11) DEFAULT NULL,
+  `Nurse` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+CREATE TABLE `users` (
+  `ID` int(11) NOT NULL,
+  `UserName` varchar(99) NOT NULL,
+  `Password` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `areas`
+--
+ALTER TABLE `areas`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indices de la tabla `calls`
+--
+ALTER TABLE `calls`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indices de la tabla `nurses`
+--
+ALTER TABLE `nurses`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indices de la tabla `patients`
+--
+ALTER TABLE `patients`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `Location` (`Location`),
+  ADD KEY `Nurse` (`Nurse`);
+
+--
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `areas`
+--
+ALTER TABLE `areas`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `calls`
+--
+ALTER TABLE `calls`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `nurses`
+--
+ALTER TABLE `nurses`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `patients`
+--
+ALTER TABLE `patients`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `users`
+--
+ALTER TABLE `users`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `patients`
+--
+ALTER TABLE `patients`
+  ADD CONSTRAINT `patients_ibfk_1` FOREIGN KEY (`Location`) REFERENCES `areas` (`ID`),
+  ADD CONSTRAINT `patients_ibfk_2` FOREIGN KEY (`Nurse`) REFERENCES `nurses` (`ID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
