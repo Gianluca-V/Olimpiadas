9fe38e2cc44b47dbcb0c75b0472d700042e5b443
Gianluca-V <vespe.gianluca@gmail.com>
2023-09-24 16:19:08 -0300

    added admin dashboard information

--- js/admin.js
+++ js/admin.js
@@ -1,18 +1,5 @@
-
 const editIcon = '<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --> <path d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z" /> </svg>';
-const deleteIcon = '<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --> <path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z" /> </svg>'
-
-// Simulate data from the server (replace this with actual API calls)
-const areasData = [
-    {
-        areaName: 'Area 1',
-        nurses: ['Nurse 1', 'Nurse 2', 'Nurse 3']
-    },
-    {
-        areaName: 'Area 2',
-        nurses: ['Nurse 4', 'Nurse 5']
-    }
-];
+const deleteIcon = '<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --> <path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z" /> </svg>';
 
 // Function to create a nurse element
 function createNurseElement(nurseName) {
@@ -23,7 +10,6 @@ function createNurseElement(nurseName) {
         <div class="areas__edit">${editIcon}</div>
         <div class="areas__delete">${deleteIcon}</div>
     `;
-    console.log(nurseElement)
     return nurseElement;
 }
 
@@ -36,7 +22,7 @@ function createAreaElement(areaData) {
     nursesContainer.classList.add('areas__nurses');
 
     areaData.nurses.forEach(nurse => {
-        const nurseElement = createNurseElement(nurse);
+        const nurseElement = createNurseElement(nurse.name);
         nursesContainer.appendChild(nurseElement);
     });
 
@@ -51,21 +37,99 @@ function createAreaElement(areaData) {
         <div class="areas__delete">${deleteIcon}</div>
     `;
 
-    // Append the nurses container to the areas container
     areaElement.querySelector('.areas__nurses').appendChild(nursesContainer);
 
     return areaElement;
 }
 
 // Function to populate areas and nurses
-function populateAreasAndNurses() {
+function populateAreasAndNurses(areasData) {
     const areasContainer = document.querySelector('.areas__container');
-    
+
     areasData.forEach((areaData) => {
         const areaElement = createAreaElement(areaData);
         areasContainer.appendChild(areaElement);
     });
 }
 
-// Call the function to populate areas and nurses when the page loads
-window.addEventListener('load', populateAreasAndNurses);
+async function fetchAreasAndNurses() {
+    try {
+        const response = await fetch('http://localhost/olimpiadasServer/api/areas/-v');
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const areasData = await response.json();
+        populateAreasAndNurses(areasData);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
+//patients section
+
+function createPatientElement(patient) {
+    const patientElement = document.createElement('div');
+    patientElement.classList.add('patients__patient');
+    patientElement.textContent = patient.FirstName + " " + patient.LastName;
+
+    return patientElement;
+}
+
+function populatePatients(patients) {
+    const patientsContainer = document.querySelector('.patients__container');
+
+    patients.forEach((patient) => {
+        const patientElement = createPatientElement(patient);
+        patientsContainer.appendChild(patientElement);
+    });
+}
+
+async function fetchPatients() {
+    try {
+        const response = await fetch('http://localhost/olimpiadasServer/api/patients/');
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const patients = await response.json();
+        populatePatients(patients);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
+//users section
+
+function createUserElement(user) {
+    const userElement = document.createElement('div');
+    userElement.classList.add('users__user');
+    userElement.textContent = user.UserName;
+
+    return userElement;
+}
+
+function populateUsers(users) {
+    const usersContainer = document.querySelector('.users__container');
+
+    users.forEach((user) => {
+        const userElement = createUserElement(user);
+        usersContainer.appendChild(userElement);
+    });
+}
+
+async function fetchUsers() {
+    try {
+        const response = await fetch('http://localhost/olimpiadasServer/api/users/');
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const users = await response.json();
+        populateUsers(users);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
+// Call the function to fetch areas and nurses data and populate the UI when the page loads
+window.addEventListener('load', ()=>{
+    Promise.all([fetchAreasAndNurses(),fetchPatients(),fetchUsers()]);
+});
